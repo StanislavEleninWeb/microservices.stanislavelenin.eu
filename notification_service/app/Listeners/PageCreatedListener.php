1ca95126f2374e0be8d490a3ff3ea3d5f1483b3f
Stanislav Elenin <stanislaveleninweb@gmail.com>
2020-12-27 21:34:03 +0200

    Modify Listener and fetch users by preference

--- notification_service/app/Listeners/PageCreatedListener.php
+++ notification_service/app/Listeners/PageCreatedListener.php
@@ -22,9 +22,22 @@ class PageCreatedListener implements ShouldQueue
      */
     public function handle(Event $event)
     {
-        // $users = User::find(collect($event->users)->pluck('id'));
-        $users = User::all();
+        $response = Http::post(env('USER_SERVICE_URL') . '/users/by/preference', [
+            'price' => $event->page['price'];
+            'price_per_square' => $event->page['price_per_square'],
+            'space' => $event->page['space'],
+            'build_type' => $event->page['build_type'],
+            'building_type' => $event->page['building_type'],
+            'region' => $event->page['region'],
+            'keywords' => $event->page['keywords'],
+        ]);
+
+        if($response->fails())
+            throw new Exception("Error Processing Request. Http request failed. Users not found", 403);
+        // Pluck user ids and return User object
+        $users = User::find(collect($response->json())->pluck('id'));
         
+        // Notify users by preference
         Notification::send($users, new PageCreatedNotification($event->page));
     }
 }
