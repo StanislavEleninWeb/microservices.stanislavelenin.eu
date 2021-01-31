1a6bbef46d6b5dbc7d8354b191fbebba70506f0f
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2021-01-31 18:47:12 +0200

    Fix api gateway failed jobs

--- api_gateway/app/Http/Controllers/FailedJobsController.php
+++ api_gateway/app/Http/Controllers/FailedJobsController.php
@@ -16,11 +16,11 @@ class FailedJobsController extends Controller
     {
         $failedJobs = [];
 
-        // $failedJobs['user.service'] = Http::get('USER_SERVICE' . 'failed/jobs');
-        $failedJobs['page.service'] = Http::get(env('PAGE_SERVICE') . 'failed/jobs');
-        // $failedJobs['image.service'] = Http::get('IMAGE_SERVICE' . 'failed/jobs');
-        // $failedJobs['rating.service'] = Http::get('RATING_SERVICE' . 'failed/jobs');
-        // $failedJobs['notification.service'] = Http::get('NOTIFICATION_SERVICE' . 'failed/jobs');
+        $failedJobs['user.service'] = Http::get(env('USER_SERVICE_URL') . 'failed/jobs');
+        $failedJobs['page.service'] = Http::get(env('PAGE_SERVICE_URL') . 'failed/jobs');
+        $failedJobs['image.service'] = Http::get(env('IMAGE_SERVICE_URL') . 'failed/jobs');
+        $failedJobs['rating.service'] = Http::get(env('RATING_SERVICE_URL') . 'failed/jobs');
+        $failedJobs['notification.service'] = Http::get(env('NOTIFICATION_SERVICE_URL') . 'failed/jobs');
 
         return response()->json($failedJobs);
     }
