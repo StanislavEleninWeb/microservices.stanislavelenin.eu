f5489dd313a95d3e55c996c8b85b35fd654cb4ea
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2021-02-01 18:53:01 +0200

    update

--- page_service/app/Console/Commands/ClearRabbitMqQueueCommand.php
+++ page_service/app/Console/Commands/ClearRabbitMqQueueCommand.php
@@ -55,7 +55,8 @@ class ClearRabbitMqQueueCommand extends Command
 
         foreach($this->queues as $queue){
             $this->callSilently('rabbitmq:queue-purge', [
-                'queue' => $queue
+                'queue' => $queue,
+                '--confirm' => true,
             ]);
         }
 
