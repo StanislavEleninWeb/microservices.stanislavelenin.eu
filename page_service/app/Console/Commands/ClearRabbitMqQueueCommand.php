efc72b966eb3aa5b33ffd4b541789014227a78ba
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2021-02-01 18:39:38 +0200

    Rabbitmq custom command fix

--- page_service/app/Console/Commands/ClearRabbitMqQueueCommand.php
+++ page_service/app/Console/Commands/ClearRabbitMqQueueCommand.php
@@ -54,7 +54,9 @@ class ClearRabbitMqQueueCommand extends Command
             $this->queues = array_intersect($this->queues, $queues);
 
         foreach($this->queues as $queue){
-            Artisan::call('rabbitmq:queue-purge ' . $queue);
+            $this->callSilently('rabbitmq:queue-purge', [
+                'queue' => $queue
+            ]);
         }
 
         $this->info('RabbitMQ queues purged!');
