f7a710d2c6358cc3b9c5cfdc844e89237b245fb2
StanislavEleninWeb <stanislaveleninweb@gmail.com>
2021-02-01 17:33:45 +0200

    Add custom rabbitmq:queue-purge command

---
+++ page_service/app/Console/Commands/ClearRabbitMqQueueCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+
+class ClearRabbitMqQueueCommand extends Command
+{
+
+    private $queues = [
+        // 'user.service',
+        'page.service',
+        'image.service',
+        'rating.service',
+        'notification.service',
+    ];
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'rabbitmq:purge {queues?*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Queue-purge recorded services';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @param  \App\Support\DripEmailer  $drip
+     * @return mixed
+     */
+    public function handle()
+    {
+        $queues = $this->argument('queues');
+
+        if(isset($queues) && count($queues))
+            $this->queues = array_intersect($this->queues, $queues);
+
+        foreach($this->queues as $queue){
+            Artisan::call('rabbitmq:queue-purge ' . $queue);
+        }
+    }
+
+}
